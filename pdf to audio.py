9225999c9857991830bc615c7132e6ccb0097bad
Deep Bag <43214783+deepbag@users.noreply.github.com>
2020-10-03 17:45:52 +0530

    pdf_name update python file

--- pdf to audio.py
+++ pdf to audio.py
@@ -1,7 +1,7 @@
 import pyttsx3
 import PyPDF2
 
-#audio function for the program
+# Audio function for the program
 def speak(audio):
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
@@ -10,14 +10,19 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-#function to read text from pdf
+# Function to read text from pdf
 def read(pdf_name,num):
     pdf_obj=PyPDF2.PdfFileReader(pdf_name)
     string=pdf_obj.getPage(num).extractText()
     speak(string)
 
-pdfname=input("Enter the name of pdf: ")
+# User Input PDF Name
+pdf_name=input("Enter the name of pdf: ")
+# User Input Page number of pdf
 page_number=int(input("Enter page number of the pdf : "))
+# Open Object for file open
 open_pdf=open(pdfname,'rb')
+# Read Object for file reading
 read(open_pdf,page_number)
+# ckose Object for file Close
 open_pdf.close()
